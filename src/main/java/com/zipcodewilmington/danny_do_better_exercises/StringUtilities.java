3a1e8660b708dfa8c547cc59251427d489cb2ebf
deepa-kakade-git <deep_303@yahoo.com>
2024-03-04 22:04:13 -0500

    StringUtilities working

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -8,7 +8,9 @@ public class StringUtilities {
      * @return `Hello World` as a string
      */
     public static String getHelloWorld() {
-        return null;
+
+        String str = "Hello World" ;
+        return str;
     }
 
     /**
@@ -17,7 +19,8 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+
+        return firstSegment + secondSegment;
     }
 
     /**
@@ -26,7 +29,8 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+
+        return firstSegment + secondSegment;
     }
 
     /**
@@ -34,7 +38,9 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+
+        return input.substring(0,Math.min(input.length(), 3));
+
     }
 
     /**
@@ -42,7 +48,8 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+
+        return input.substring(Math.min(input.length(), 2), input.length());
     }
 
     /**
@@ -50,8 +57,12 @@ public class StringUtilities {
      * @param comparableValue the value to be compared against
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
-    public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+    public static Boolean compareTwoStrings(String inputValue, String comparableValue) {
+        if (inputValue.equals(comparableValue)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -59,7 +70,17 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        int len = inputValue.length();
+        char[] charArray = inputValue.toCharArray();
+        int middle = len/2;
+        if (len % 2 == 0) { // Even length
+            // Return two middle characters
+            return charArray[middle- 1]; // For even length, returns the character before the exact middle
+        } else { // Odd length
+            // Return the middle character
+            return charArray[middle];
+        }
+
     }
 
     /**
@@ -67,7 +88,7 @@ public class StringUtilities {
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+        return spaceDelimitedString.split("\\s+")[0];
     }
 
     /**
@@ -75,7 +96,7 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        return spaceDelimitedString.split("\\s+")[1];
     }
 
     /**
@@ -83,6 +104,21 @@ public class StringUtilities {
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+        char[] charArray = stringToReverse.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+
+        while (start < end) {
+            char temp = charArray[start]; //first character of string is assigned to temp
+            charArray[start] = charArray[end]; //charArray[0] = charArray[4] last char of string is saved to first
+            charArray[end] = temp;
+            start++;
+            end--;
+        }
+
+        String reversed = new String(charArray);
+        return reversed;
+
+
     }
 }
